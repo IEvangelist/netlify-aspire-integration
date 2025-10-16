9f92e09e88e329636b80761fa5b2b65a70f3a2e9
David Pine <david.pine@microsoft.com>
2025-10-15 21:31:05 -0500

    feat: Add build steps for multiple frontend applications in Aspire deploy workflow

--- src/Netlify.AppHost/AppHost.cs
+++ src/Netlify.AppHost/AppHost.cs
@@ -16,6 +16,7 @@ builder.AddNpmApp("astro", "../astro", "dev")
         options: new NetlifyDeployOptions()
         {
             Dir = "dist",
+            NoBuild = true,
             Site = "eace4cd9-35ba-4076-9129-5b54418b7512"
         },
         authToken: authToken);
@@ -28,6 +29,7 @@ builder.AddNpmApp("react", "../react", "dev")
         options: new NetlifyDeployOptions()
         {
             Dir = "dist",
+            NoBuild = true,
             Site = "e936ef36-d1fa-46b2-98d5-8e61b4459330"
         },
         authToken: authToken);
@@ -40,6 +42,7 @@ builder.AddNpmApp("vue", "../vue", "dev")
         options: new NetlifyDeployOptions()
         {
             Dir = "dist",
+            NoBuild = true,
             Site = "a8f5b603-a496-499a-b575-f8786f9456a5"
         },
         authToken: authToken);
@@ -52,6 +55,7 @@ builder.AddNpmApp("angular", "../angular", "start")
         options: new NetlifyDeployOptions()
         {
             Dir = "dist/angular/browser",
+            NoBuild = true,
             Site = "73d17585-2fe2-4bda-84d8-ad503cec7cab"
         },
         authToken: authToken);
@@ -64,6 +68,7 @@ builder.AddNpmApp("svelte", "../svelte", "dev")
         options: new NetlifyDeployOptions()
         {
             Dir = "dist",
+            NoBuild = true,
             Site = "de97e199-3b64-4824-b838-81ee33e13976"
         },
         authToken: authToken);
@@ -76,6 +81,7 @@ builder.AddNpmApp("next", "../next", "dev")
         options: new NetlifyDeployOptions()
         {
             Dir = "out",
+            NoBuild = true,
             Site = "79a9e3b6-337e-4008-88c4-883bc53c90bc"
         },
         authToken: authToken);
