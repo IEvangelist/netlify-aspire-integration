558ba0059aea180850194c4f310a4214f539e473
David Pine <david.pine@microsoft.com>
2025-10-15 21:04:42 -0500

    feat: Update Netlify deployment options for various apps to include site IDs

--- src/Netlify.AppHost/AppHost.cs
+++ src/Netlify.AppHost/AppHost.cs
@@ -1,10 +1,11 @@
 // Copyright (c) David Pine. All rights reserved.
 // Licensed under the MIT License.
-
 var builder = DistributedApplication.CreateBuilder(args);
 
 var authToken = builder.AddParameterFromConfiguration(
    "netlify-token", "NETLIFY_AUTH_TOKEN", secret: true);
+
+var createSites = true;
 
 builder.Pipeline.AddNetlifyDeployPipeline();
 
@@ -13,7 +14,11 @@ builder.AddNpmApp("astro", "../astro", "dev")
     .WithNpmPackageInstallation()
     .WithHttpEndpoint(targetPort: 4321, env: "PORT")
     .PublishAsNetlifySite(
-        options: new NetlifyDeployOptions() { Dir = "dist" },
+        options: new NetlifyDeployOptions()
+        {
+            Dir = "dist",
+            Site = "eace4cd9-35ba-4076-9129-5b54418b7512"
+        },
         authToken: authToken);
 
 // React app - Vite + TypeScript
@@ -21,7 +26,11 @@ builder.AddNpmApp("react", "../react", "dev")
     .WithNpmPackageInstallation()
     .WithHttpEndpoint(targetPort: 5173, env: "PORT")
     .PublishAsNetlifySite(
-        options: new NetlifyDeployOptions() { Dir = "dist" },
+        options: new NetlifyDeployOptions()
+        {
+            Dir = "dist",
+            Site = "e936ef36-d1fa-46b2-98d5-8e61b4459330"
+        },
         authToken: authToken);
 
 // Vue app - Vite
@@ -29,7 +38,11 @@ builder.AddNpmApp("vue", "../vue", "dev")
     .WithNpmPackageInstallation()
     .WithHttpEndpoint(targetPort: 5174, env: "PORT")
     .PublishAsNetlifySite(
-        options: new NetlifyDeployOptions() { Dir = "dist" },
+        options: new NetlifyDeployOptions()
+        {
+            Dir = "dist",
+            Site = "a8f5b603-a496-499a-b575-f8786f9456a5"
+        },
         authToken: authToken);
 
 // Angular app - Angular CLI
@@ -37,7 +50,11 @@ builder.AddNpmApp("angular", "../angular", "start")
     .WithNpmPackageInstallation()
     .WithHttpEndpoint(targetPort: 4200, env: "PORT")
     .PublishAsNetlifySite(
-        options: new NetlifyDeployOptions() { Dir = "dist/angular/browser", CreateSite = "angular" },
+        options: new NetlifyDeployOptions()
+        {
+            Dir = "dist/angular/browser",
+            Site = "73d17585-2fe2-4bda-84d8-ad503cec7cab"
+        },
         authToken: authToken);
 
 // Svelte app - Vite + TypeScript
@@ -45,7 +62,11 @@ builder.AddNpmApp("svelte", "../svelte", "dev")
     .WithNpmPackageInstallation()
     .WithHttpEndpoint(targetPort: 5175, env: "PORT")
     .PublishAsNetlifySite(
-        options: new NetlifyDeployOptions() { Dir = "dist", CreateSite = "svelte" },
+        options: new NetlifyDeployOptions()
+        {
+            Dir = "dist",
+            Site = "de97e199-3b64-4824-b838-81ee33e13976"
+        },
         authToken: authToken);
 
 // Next.js app - Static Export (requires next.config.js with output: 'export')
@@ -53,7 +74,11 @@ builder.AddNpmApp("next", "../next", "dev")
     .WithNpmPackageInstallation()
     .WithHttpEndpoint(targetPort: 3000, env: "PORT")
     .PublishAsNetlifySite(
-        options: new NetlifyDeployOptions() { Dir = "out" },
+        options: new NetlifyDeployOptions()
+        {
+            Dir = "out",
+            Site = "79a9e3b6-337e-4008-88c4-883bc53c90bc"
+        },
         authToken: authToken);
 
 builder.Build().Run();
